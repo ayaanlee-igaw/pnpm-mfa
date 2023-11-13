f0f2ee60b6df993dbe94664239a8a5a267c532e6
ayaanlee-igaw <ayaan.lee@igaworks.com>
2023-11-13 10:05:41 +0900

    조건부 설정 vite

--- packages/container/vite.config.ts
+++ packages/container/vite.config.ts
@@ -4,39 +4,38 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import federation from '@originjs/vite-plugin-federation';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  // server: {
-  //   port: 5000
-  // },
-  // yarn berry 사용하는 경우 node module 생성하지 않기 위함
-  // cacheDir: './.vite',
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  plugins: [
-    vue(),
-    federation({
-      name: 'container',
-      remotes: {
-        service: 'http://localhost:5001/assets/remoteEntry.js'
-      },
-      shared: ['vue', 'pinia', 'vue-router'],
-    }),
-  ],
-  build: {
-    modulePreload: false,
-    target: "esnext",
-    minify: false,
-    // cssCodeSplit: false,
-    // rollupOptions: {
-    //   output: {
-    //     format: 'esm',
-    //     entryFileNames: 'assets/[name].js',
-    //     minifyInternalExports: false,
-    //   },
-    // },
-  },
+export default defineConfig(({ command }) => {
+  // 조건부 설정
+  // https://ko.vitejs.dev/config/#conditional-config
+  const federation_service = command === 'serve' ? 'http://localhost:5001/dist/assets/remoteEntry.js' : 'http://localhost:5001/assets/remoteEntry.js';
+  return {
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    plugins: [
+      vue(),
+      federation({
+        name: 'container',
+        remotes: {
+          service: federation_service
+        },
+        shared: ['vue', 'pinia', 'vue-router'],
+      }),
+    ],
+    build: {
+      modulePreload: false,
+      target: "esnext",
+      minify: false,
+      // cssCodeSplit: false,
+      // rollupOptions: {
+      //   output: {
+      //     format: 'esm',
+      //     entryFileNames: 'assets/[name].js',
+      //     minifyInternalExports: false,
+      //   },
+      // },
+    },
+  }
 })
